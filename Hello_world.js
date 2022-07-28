cd7e530fae91b29e0622e5d03cbd8850254d45ef
stefan <stefanbayne@gmail.com>
2022-07-28 19:55:00 -0400

    comments added

--- Hello_world.js
+++ Hello_world.js
@@ -1,3 +1,5 @@
+// This is how we would print some information to the screen.
+// console.log() is the same as println() in Kotlin for Android.
 console.log('Hello World!')
 console.log('This is my first JavaScript program!')
 console.log('Hello again!')
