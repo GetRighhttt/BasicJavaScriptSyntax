d4227a54046a8573a74fca68ab5a9924e3a1b663
stefan <stefanbayne@gmail.com>
2022-07-28 22:59:39 -0400

    Switch Statement

---
+++ Switch.js
@@ -0,0 +1,16 @@
+/**
+ * Switch statement in JavaScript is like When in Kotlin.
+ */
+
+var names = "Example"
+
+switch(names) {
+    case "A":
+        console.log('Male');
+        break;
+    case "B":
+        console.log('Female');
+        break;
+    default:
+        console.log('Unknown');
+}
