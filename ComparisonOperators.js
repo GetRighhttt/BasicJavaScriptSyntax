3dcf02d0dd8119c85a95cbb8fc5c07e5ff319be3
stefan <stefanbayne@gmail.com>
2022-07-28 22:46:41 -0400

    Comparison Operators

---
+++ ComparisonOperators.js
@@ -0,0 +1,18 @@
+/**
+ * These are the comparison operators that JavaScript has to offer.
+ * 
+ * 
+ */
+
+// Comparison Operators
+// < <= > >= == !=
+// the return = a boolean is either true or false
+console.log(10 == 10) // equal to
+console.log(10 != 10) // not equal
+console.log(10 < 10) // less
+console.log(10 <= 10) // less than or equal to
+console.log(10 > 10) // greater than
+console.log(10 >= 10) // greater than or equal to
+
+console.log("A" == "B")
+console.log("A" != "B")
