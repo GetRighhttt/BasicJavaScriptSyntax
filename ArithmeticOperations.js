05413f333fd1ad754c7730cce53be7bbadd2fb98
stefan <stefanbayne@gmail.com>
2022-07-28 21:23:49 -0400

    Basic Math

---
+++ ArithmeticOperations.js
@@ -0,0 +1,20 @@
+/**
+ * Just Basic Math Operations in JavaScript.
+ */
+
+var addition = 2 + 2
+var subtraction = 3 - 1
+var division = 10 / 2 
+var multiplication = 8 * 8
+var modulus = 100 % 2
+var exponent = 3 ** 3
+
+console.log(addition) // prints 4
+console.log(subtraction) // prints 2
+console.log(division) // prints 5
+console.log(multiplication) // prints 64
+console.log(modulus) // remainder of 0
+console.log(exponent) // prints 27
+console.log("End of operations.")
+
+console.log((2 + 3) / 1)
