0bdd464a6097d13f942280733829a97416a29c6c
stefan <stefanbayne@gmail.com>
2022-07-28 20:00:36 -0400

    final commit with logging basic math operations

--- Hello_world.js
+++ Hello_world.js
@@ -1,6 +1,10 @@
 // This is how we would print some information to the screen.
 // console.log() is the same as println() in Kotlin for Android.
+// JavaScript doesn't enforce semicolons or double quotes for strings.
+// JavaScript also doesn't have variable types
 console.log('Hello World!')
 console.log('This is my first JavaScript program!')
 console.log('Hello again!')
 console.log('This is the autosave!')
+console.log(20 + 10)
+console.log(20-10)
