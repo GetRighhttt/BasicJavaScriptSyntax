e116004ebabc5e740cfc57bbe6c2530bb37cd638
stefan <stefanbayne@gmail.com>
2022-07-28 22:48:58 -0400

    Logical Operators

--- ComparisonOperators.js
+++ ComparisonOperators.js
@@ -1,7 +1,7 @@
 /**
  * These are the comparison operators that JavaScript has to offer.
  * 
- * 
+ * We also go over logical operators towards the end of the file.
  */
 
 // Comparison Operators
@@ -14,5 +14,12 @@ console.log(10 <= 10) // less than or equal to
 console.log(10 > 10) // greater than
 console.log(10 >= 10) // greater than or equal to
 
-console.log("A" == "B")
+console.log("A" == "B") 
 console.log("A" != "B")
+
+// These are logical operators which evaluate logical expressions.
+// allows us to connect two or more expressions
+// Logical Operators
+// && ||
+console.log(10 == 10 && "A" == "A")
+console.log(10 == 10 || "A" == "B")
