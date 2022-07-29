6624a4f77fed9d07d9ff28ad6af7ff09cb22e0c1
stefan <stefanbayne@gmail.com>
2022-07-28 20:10:33 -0400

    variables examples

---
+++ Variables.js
@@ -0,0 +1,26 @@
+// Variable = placeholder to star any value
+// JavaScript uses 'var' for variables
+var firstName = "Stefan"
+console.log(firstName)
+
+var age = 27
+console.log(age)
+
+var sex = "Male"
+console.log(sex)
+
+var isMale = true
+console.log(isMale)
+
+// We can store dates by using 'new Date()'
+var birthday = new Date(1995, 0, 6)
+console.log(birthday)
+
+// We can also have objects
+var dog = {}
+console.log(dog)
+
+// We can have empty variables too using 'undefined'.. 
+// kind of like nullables in Kotlin
+var empty = undefined
+console.log(empty)
