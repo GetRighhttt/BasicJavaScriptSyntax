3214dff4c5c9817dfbcd53d9b131533e7f5e417c
stefan <stefanbayne@gmail.com>
2022-07-28 23:20:02 -0400

    Why we use const.

--- Const.js
+++ Const.js
@@ -2,6 +2,9 @@
  * Const keyword is used to declare a constant. 
  * 
  * It's bad practice to consistently change variable types.
+ * 
+ * Recommended practice is to use the const keyword for variable declaration
+ * in JavaScript. 
  */
 
 // This is an example of bad practice in coding.
