dfc8d1b13bf3ce187044707b145829501b1adb65
stefan <stefanbayne@gmail.com>
2022-07-28 20:13:00 -0400

    added datatypes

--- Variables.js
+++ Variables.js
@@ -1,15 +1,16 @@
 // Variable = placeholder to star any value
 // JavaScript uses 'var' for variables
-var firstName = "Stefan"
+// JavaScript like Kotlin has type inference
+var firstName = "Stefan" // string
 console.log(firstName)
 
-var age = 27
+var age = 27 // int
 console.log(age)
 
-var sex = "Male"
+var sex = "Male" // string
 console.log(sex)
 
-var isMale = true
+var isMale = true // boolean
 console.log(isMale)
 
 // We can store dates by using 'new Date()'
@@ -17,7 +18,7 @@ var birthday = new Date(1995, 0, 6)
 console.log(birthday)
 
 // We can also have objects
-var dog = {}
+var dog = {} // object
 console.log(dog)
 
 // We can have empty variables too using 'undefined'.. 
